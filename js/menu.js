4596c58f2743bd82381a6fe40dfa345e0d437f17
Maxcabsa <89762628+Maxcabsa@users.noreply.github.com>
2021-09-05 22:05:12 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,13 @@ var nav = document.getElementById('nav');
 btnMenu.addEventListener('click', function(){
     nav.classList.toggle('mostrar');
 })
+var inputs = document.getElementsByClassName('formulario_input-text')
+for (var i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('keyup', function(){
+        if(this.value.length>=1){
+                this.nextElementSibling.classList.add('fijar');
+        } else {
+            this.nextElementSibling.classList.remove('fijar')
+        }
+    })
+}
