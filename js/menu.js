d40967bfd30eb16d62066fd30e55e551a037d0da
Maxcabsa <89762628+Maxcabsa@users.noreply.github.com>
2021-09-06 00:27:47 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -13,3 +13,23 @@ for (var i = 0; i < inputs.length; i++){
         }
     })
 }
+function enviar(){
+var nombre, correo, telefono1, telefono2, curso;
+nombre= document.getElementById("nombre").value;
+correo= document.getElementById("correo").value;
+telefono1= document.getElementById("telefono1").value;
+telefono2= document.getElementById("telefono2").value;
+curso= document.getElementById("curso").value;
+    if(nombre === "" || correo === "" || telefono1 === "" || telefono2 === "" || curso === ""){
+    alert ("Todos los campos son obligatorios");
+    return false;
+    }
+    else if(nombre.length>30 || correo.length>40 || telefono1.length>12 || telefono2.length>12 || curso.length>30){
+    alert("demasiados carácteres");
+    return false;   
+    }
+    else{
+        alert("Gracias por su registro, nos pondremos en contacto con usted")
+    
+    }
+}
