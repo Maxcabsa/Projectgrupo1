c159c01d756cc4956b8710467708f90898b33b10
Maxcabsa <89762628+Maxcabsa@users.noreply.github.com>
2023-07-21 17:22:48 -0500

    Add files via upload

---
+++ sesion 6/eje1.js
@@ -0,0 +1,4 @@
+document.write("Bienvenida a mi programa <br>");
+document.write("Introduccion a la computacion <br>");
+document.write("Caballero Sanchez Max Erick <br>");
+document.write("jr. Argentina")
---
+++ sesion 6/eje2.js
@@ -0,0 +1,10 @@
+var nombre, edad, notafinal;
+nombre="Max Erick";
+edad=19;
+notafinal=16;
+document.write("Su nombre es: " +nombre+"<br>");
+document.write("Su edad es: " + edad +"<br>");
+document.write("Su nota final es: " + notafinal+"<br>");
+
+
+
