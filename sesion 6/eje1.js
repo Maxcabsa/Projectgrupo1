98fb5bc24155556497f13babf6a1f7d3d399421c
Maxcabsa <89762628+Maxcabsa@users.noreply.github.com>
2024-04-26 19:06:02 -0500

    Delete sesion 6 directory

--- sesion 6/eje1.js
+++
@@ -1,4 +0,0 @@
-document.write("Bienvenida a mi programa <br>");
-document.write("Introduccion a la computacion <br>");
-document.write("Caballero Sanchez Max Erick <br>");
-document.write("jr. Argentina")
